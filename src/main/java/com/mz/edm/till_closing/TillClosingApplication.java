981049f68274801442670899feead71657b9609c
Luis gonzalez <luisworker2010@gmail.com>
2024-07-16 16:01:08 +0200

    first commit

---
+++ src/main/java/com/mz/edm/till_closing/TillClosingApplication.java
@@ -0,0 +1,13 @@
+package com.mz.edm.till_closing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TillClosingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TillClosingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mz/edm/till_closing/TillClosingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mz.edm.till_closing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TillClosingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
